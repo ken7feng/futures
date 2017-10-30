e548e705afeaa582c2bc7e733d9f8230df563773
ken7feng <124224540@qq.com>
2017-10-30 11:07:18 +0800

    创建项目

---
+++ WEXFutures/WEXFutures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WEXFutures
+//
+//  Created by K on 2017/10/30.
+//  Copyright © 2017年 K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WEXFutures/WEXFutures/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WEXFutures
+//
+//  Created by K on 2017/10/30.
+//  Copyright © 2017年 K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
